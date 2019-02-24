4db28ed1709328b1f9c05134d23dc6fb25129b92
pke <praveenkumar199324@gmail.com>
2019-02-24 04:35:09 -0600

    First-Functional-Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App Component</h1>
+        <Header />
         <Contact />
       </div>
     );
