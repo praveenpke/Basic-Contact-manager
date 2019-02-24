4e1e0497150548f1e8dafe7ebaa10771277532b5
pke <praveenkumar199324@gmail.com>
2019-02-24 04:32:29 -0600

    First-component-created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-
 import "./App.css";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <h1>App Component</h1>
+        <Contact />
+      </div>
+    );
   }
 }
 
