de2d5604ec0935785b95d97de9ec882bcf60b816
pke <praveenkumar199324@gmail.com>
2019-02-24 23:31:40 -0600

    Setup Prop Type
    Setting up property type to string so that no other type cant be passed as props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
   return (
@@ -10,6 +11,10 @@ const Header = props => {
 
 Header.defaultProps = {
   branding: "My App"
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired //setting up prop type to be string
 };
 
 export default Header;
