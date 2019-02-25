629bfdefc942709501a7cc0aea10f4ffdc907393
pke <praveenkumar199324@gmail.com>
2019-02-25 03:14:43 -0600

    click event handling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,19 +9,63 @@ export class Contact extends Component {
   //   phone: PropTypes.string.isRequired
   // };
 
+  state = {
+    showCard: false
+  };
+
+  showContact = () => {
+    this.setState({
+      showCard: true
+    });
+    console.log(this.state);
+  };
+
+  hideContact = () => {
+    this.setState({
+      showCard: false
+    });
+    console.log(this.state);
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
+        <h4>
+          {name}
+          <span>
+            {" "}
+            {!this.state.showCard ? (
+              <i
+                style={linkCursorStyle}
+                onClick={this.showContact}
+                className="fas fa-chevron-circle-down"
+              />
+            ) : (
+              <i
+                style={linkCursorStyle}
+                onClick={this.hideContact}
+                className="fas fa-chevron-circle-up"
+              />
+            )}
+          </span>
+        </h4>
+        {this.state.showCard ? (
+          <ul className="list-group">
+            <li className="list-group-item bg-light">{email}</li>
+            <li className="list-group-item bg-light">{phone}</li>
+          </ul>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
 }
+
+const linkCursorStyle = {
+  cursor: "pointer"
+};
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired
